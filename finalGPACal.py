fb5c8966825b8b8a88fb8ada4c47b50b893accd2
jgranger3 <163935841+jgranger3@users.noreply.github.com>
2024-07-14 18:48:37 -0400

    Add files via upload
    So, far I have finished the outlining of the GPA calculator, I still have a lot to go.

---
+++ finalGPACal.py
@@ -0,0 +1,20 @@
+"""
+Author: Juanita Granger
+Assignment: M08 Final Project
+Version 1.0 07/14/24
+This program is suppose to help users calculate the
+GPA based off their current grades.
+"""
+
+from tkinter import*
+
+def main():
+    root = Tk()
+    root.title("GPA Calculator")
+    root.geometry("400x400")
+    GPA = ""
+
+
+    root.mainloop()
+
+main()
